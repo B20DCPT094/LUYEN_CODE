cce85d8c84ecf443367aa4650a5e93367b85e098
B20DCPT094 <wuochuy02@gmail.com>
2022-11-23 18:03:25 +0700

    test

---
+++ RANGED.cpp
@@ -0,0 +1,13 @@
+int main() {
+	long long A, B, C, D;
+	cin >> A >> B >> C >> D;
+	if (A > B)
+		swap(A, B);
+	if (C > D)
+		swap(C, D);
+	if (B < C || A > D)
+		cout << "NO";
+	else
+		cout << "YES";
+
+}
